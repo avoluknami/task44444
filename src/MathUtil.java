53ad21160c91daa2b72abae7988d08c5e7687310
aNABEL <user@Noutbuk-user.local>
2024-09-24 14:53:09 -0700

    first commit

---
+++ src/MathUtil.java
@@ -0,0 +1,13 @@
+public class MathUtil {
+
+    public static boolean isPrime(int n){
+
+    }
+
+    public static boolean gcd(int a,int b){
+
+    }
+
+    public static boolean lcm(int a, int b)
+
+}
